add7c279a79fcfa17706a13174ef38bd52b333ea
Milan Souček <milan.soucek.2004@gmail.com>
2024-04-17 13:58:02 +0200

    comment added

--- server/abl/transaction/listAbl.js
+++ server/abl/transaction/listAbl.js
@@ -1,6 +1,7 @@
 const transactionDao = require("../../dao/transaction-dao.js");
 const userDao = require("../../dao/user-dao.js"); // Assuming you have a user DAO
 
+//doest work yet
 async function ListAbl(req, res) {
   try {
     const transactionList = transactionDao.list();
