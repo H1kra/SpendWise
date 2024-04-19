71750fa1b1e833e284b77ea892e5c5f0a82de922
Milan Souček <milan.soucek.2004@gmail.com>
2024-04-19 10:22:46 +0200

    repaired userController delete, added bcryptjs module

--- server/controller/user.js
+++ server/controller/user.js
@@ -5,7 +5,7 @@ const GetAbl = require("../abl/user/getAbl");
 const ListAbl = require("../abl/user/listAbl");
 const CreateAbl = require("../abl/user/createAbl");
 const UpdateAbl = require("../abl/user/updateAbl");
-const DeleteAbl = require("../abl/user/getAbl");
+const DeleteAbl = require("../abl/user/deleteAbl");
 
 router.get("/get", (req, res) => {
   GetAbl(req, res);
